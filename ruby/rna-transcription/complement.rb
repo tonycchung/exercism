84d0288919199da85f2f27096ee4f14af8f4bd50
Tony Chung <anthonyuchung@gmail.com>
2014-09-12 15:39:05 -0700

    Refactor rna-transcription again - zip two arrays to create hash. Just trying other ways to do things.

--- ruby/rna-transcription/complement.rb
+++ ruby/rna-transcription/complement.rb
@@ -1,17 +1,23 @@
 class Complement
 
   def self.of_dna(dna)
-    hsh = {G: 'C', C: 'G', T: 'A', A: 'U'}
+    keys = %w(G C T A)
+    values = %w(C G A U)
+    hsh = keys.zip(values).to_h
+
     dna.split("").each_index do |i|
-      dna[i] = hsh[dna[i].to_sym]
+      dna[i] = hsh[dna[i]]
     end
     dna
   end
 
   def self.of_rna(rna)
-    hsh = {G: 'C', C: 'G', A: 'T', U: 'A'}
+    keys = %w(G C A U)
+    values = %w(C G T A)
+    hsh = keys.zip(values).to_h
+
     rna.split("").each_index do |i|
-      rna[i] = hsh[rna[i].to_sym]
+      rna[i] = hsh[rna[i]]
     end
     rna
   end
