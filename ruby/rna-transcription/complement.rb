51d721ebc18d1df31da4225df4ab313b66787513
Tony Chung <anthonyuchung@gmail.com>
2014-09-17 14:48:20 -0700

    Refactor rna-transcription - so much simpler usering String.tr

--- ruby/rna-transcription/complement.rb
+++ ruby/rna-transcription/complement.rb
@@ -1,24 +1,14 @@
 class Complement
 
-  def self.of_dna(dna)
-    keys = %w(G C T A)
-    values = %w(C G A U)
-    hsh = keys.zip(values).to_h
+  DNA = "GCTA"
+  RNA = "CGAU"
 
-    dna.split("").inject("") do |result, element|
-      result << hsh[element]
-      result
-    end
+  def self.of_dna(dna)
+    dna.tr(DNA, RNA)
   end
 
   def self.of_rna(rna)
-    keys = %w(G C A U)
-    values = %w(C G T A)
-    hsh = keys.zip(values).to_h
-
-    rna.split("").inject("") do |result, element|
-      result << hsh[element]
-      result
-    end
+    rna.tr(RNA, DNA)
   end
+
 end
