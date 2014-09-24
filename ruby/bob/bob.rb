49764505bb584853a51939aea2a267698dd22a08
Tony Chung <anthonyuchung@gmail.com>
2014-09-23 17:17:35 -0700

    Refactor bob - create helper functions for readability.

--- ruby/bob/bob.rb
+++ ruby/bob/bob.rb
@@ -1,13 +1,27 @@
 class Bob
   def hey(msg)
-    if msg.scan(/[a-zA-Z]+/) == msg.scan(/[A-Z]+/) && !msg.scan(/[A-Z]+/).empty?
+    if yelling?(msg)
       return 'Woah, chill out!'
-    elsif msg[-1] == '?'
+    elsif question?(msg)
       return 'Sure.'
-    elsif msg.split.empty?
+    elsif silent?(msg)
       return 'Fine. Be that way!'
     else
       return'Whatever.'
     end
+  end
+
+  private
+
+  def yelling?(msg)
+    msg.scan(/[a-zA-Z]+/) == msg.scan(/[A-Z]+/) && !msg.scan(/[A-Z]+/).empty?
+  end
+
+  def question?(msg)
+    msg[-1] == '?'
+  end
+
+  def silent?(msg)
+    msg.split.empty?
   end
 end
