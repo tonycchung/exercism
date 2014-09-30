d49bc2eec09ae534b751bf089d6102a19dfdb320
Tony Chung <anthonyuchung@gmail.com>
2014-09-29 18:47:01 -0700

    Complete beer song. Simple mistake similar to food chain.

--- ruby/beer-song/beer_song.rb
+++ ruby/beer-song/beer_song.rb
@@ -7,22 +7,14 @@ class BeerSong
   end
 
   def verses(start, finish)
+    result = ""
     start.downto(finish) do |num|
-      verse(num)
+      result += verse(num) + "\n"
     end
+    result
   end
 
   def sing
     verses(99, 0)
   end
 end
-
-
-# 2 bottles of beer on the wall, 2 bottles of beer.
-# Take one down and pass it around, 1 bottle of beer on the wall.
-
-# 1 bottle of beer on the wall, 1 bottle of beer.
-# Take it down and pass it around, no more bottles of beer on the wall.
-
-# No more bottles of beer on the wall, no more bottles of beer.
-# Go to the store and buy some more, 99 bottles of beer on the wall.
