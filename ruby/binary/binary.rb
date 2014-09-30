65528d59d8114160df3bafa41053ce52f40602ac
Tony Chung <anthonyuchung@gmail.com>
2014-09-29 18:22:00 -0700

    Complete trinary. Refactor binary as well.

--- ruby/binary/binary.rb
+++ ruby/binary/binary.rb
@@ -1,14 +1,14 @@
 class Binary
 
   def initialize(num)
-    @places = num.size
+    @places = num.size - 1
     @num = num
   end
 
   def to_decimal
     return 0 if @num.to_i == 0
     @num.split("").each_with_index.inject(0) do |result, (digit, i)|
-      result += digit.to_i * 2**(@places - i-1)
+      result += digit.to_i * 2**(@places - i)
       result
     end
   end
