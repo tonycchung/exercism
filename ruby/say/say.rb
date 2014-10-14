37d6ae17b42547bd8e17fa39ff6f1187922729f8
Tony Chung <anthonyuchung@gmail.com>
2014-10-14 16:07:40 -0700

    Complete say. Solution by darcnite3000. Love the recursion and readability.

--- ruby/say/say.rb
+++ ruby/say/say.rb
@@ -1,44 +1,95 @@
 class Say
 
+  TENS = %W[
+      zero
+      ten
+      twenty
+      thirty
+      forty
+      fifty
+      sixty
+      seventy
+      eighty
+      ninety
+    ]
+
+  TEENS = %w[
+      zero
+      one
+      two
+      three
+      four
+      five
+      six
+      seven
+      eight
+      nine
+      ten
+      eleven
+      twelve
+      thirteen
+      fourteen
+      fifteen
+      sixteen
+      seventeen
+      eighteen
+      nineteen
+    ]
+
+
   def initialize(number)
-    @number = number.to_s
-    @ones = %w(0 1 2 3 4 5 6 7 8 9).map(&:to_i).zip(%w(zero one two three four five six seven eight nine)).to_h
-    @teens = %w(10 11 12 13 14 15 16 17 18 19).map(&:to_i).zip(%w(ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen)).to_h
-    @tens = %w(20 30 40 50 60 70 80 90 100).map(&:to_i).zip(%w(twenty thirty forty fifty sixty seventy eighty ninety hundred)).to_h
-    @thousands = %w(thousand million billion)
-  end
-
-  def chunk
-    raise 'Number out of range!' if @number.size > 999_999_999_999 || @number.to_i < 0
-    @number.reverse.split('').each_slice(3).to_a.map(&:reverse).reverse.map(&:join).map(&:to_i)
-    # ['1', '234', '567']
-  end
-
-  def half_english
-    result = ''
-    @chunk = chunk
-    @chunk.each_with_index do |number, i|
-      thousandth_index = @chunk.size - 2 - i
-      result += "#{number} #{@thousands[thousandth_index] if @chunk.size > i + 1} "
-    end
-    result.strip
+    @number = number
   end
 
   def in_english
-    half = half_english.split(' ')
-    half.each_with_index do |num, i|
-      key = num.to_i
-      result = ''
-      if key.between?(0,9)
-        result += @ones[key]
-      elsif key.between?(10, 19)
-        result += @teens[key]
-      elsif key.between?(11, 20)
-        result += @tens[key]
+    @english ||= to_english @number
+  end
+
+  private
+  def to_english(number)
+    raise ArgumentError unless number.between?(0,10**12)
+
+    case number
+    when (0...20)
+      TEENS[number]
+    when (20...100)
+      tens, rest = number.divmod 10
+      if rest.zero?
+        TENS[tens]
+      else
+        "#{TENS[tens]}-#{to_english rest}"
       end
-      half[i] = result
+    when (100...1000)
+      hundreds, rest = number.divmod 100
+      if rest.zero?
+        "#{to_english hundreds} hundred"
+      else
+        "#{to_english hundreds} hundred #{to_english rest}"
+      end
+    when (1000...10**6)
+      thousands, rest = number.divmod 1000
+      if rest.zero?
+        "#{to_english thousands} thousand"
+      else
+        "#{to_english thousands} thousand #{to_english rest}"
+      end
+    when (10**6...10**9)
+      millions, rest = number.divmod 10**6
+      if rest.zero?
+        "#{to_english millions} million"
+      else
+        "#{to_english millions} million #{to_english rest}"
+      end
+    when (10**9...10**12)
+      billions, rest = number.divmod 10**9
+      if rest.zero?
+        "#{to_english billions} billion"
+      else
+        "#{to_english billions} billion #{to_english rest}"
+      end
+    else
+      raise ArgumentError
     end
-    half.join(' ')
   end
 
 end
